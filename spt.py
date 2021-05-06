b06618da695c60dafd37f295726d190a82b5c46a
Brabitt <ronnyanchaluisa@gmail.com>
2021-05-06 14:50:13 +0200

    defined functions for the languages

--- spt.py
+++ spt.py
@@ -10,27 +10,45 @@ mic = sr.Microphone(device_index=0)
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 
-# Spanish Voice
-es_voice_id = "com.apple.speech.synthesis.voice.monica"
-engine.setProperty('voice', es_voice_id)
+
+def speak_es(audio):
+    """
+    Spanish voice
+    :param audio:
+    :return: string
+    """
+
+    es_voice_id = "com.apple.speech.synthesis.voice.monica"
+    engine.setProperty('voice', es_voice_id)
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def speak_de(audio):
+    """
+    German voice
+    :param audio:
+    :return: string
+    """
+    de_voice_id = "com.apple.speech.synthesis.voice.anna"
+    engine.setProperty('voice', de_voice_id)
+    engine.say(audio)
+    engine.runAndWait()
+
 
 with mic as source:
     # Speak the assistant with the Spanish Voice
-    engine.say('Hola, dime lo que quieres traducir')
-    engine.runAndWait()
+    speak_es('Hola, dime lo que quieres traducir')
     # listen the audio via source
-    r.adjust_for_ambient_noise(source)
+    r.adjust_for_ambient_noise(source, 5)
     r.pause_threshold = 1
     audio = r.listen(source)
     # define the input language (Spanish)
     result = r.recognize_google(audio, language='es')
-    engine.say('quieres traducir: ' + result + 'en aleman')
-    engine.runAndWait()
+    speak_es('quieres traducir: ' + result + 'en aleman')
     # initialize the translator
     p = google_translator()
     k = p.translate(result, lang_tgt='de')
     # German  Voice
-    de_voice_id = "com.apple.speech.synthesis.voice.anna"
-    engine.setProperty('voice', de_voice_id)
-    engine.say(k)
-    engine.runAndWait()
+    speak_de(k)
+
