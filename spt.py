86b9048033724790fce71e99b003e78ecfd71a2f
Brabitt <ronnyanchaluisa@gmail.com>
2021-05-08 12:29:07 +0200

    added mic function

--- spt.py
+++ spt.py
@@ -1,6 +1,10 @@
+import datetime
+
 import speech_recognition as sr
 from google_trans_new import google_translator
 import pyttsx3
+from time import sleep
+
 
 # initialize the microphone of the computer
 r = sr.Recognizer()
@@ -8,47 +12,60 @@ mic = sr.Microphone(device_index=0)
 
 # initialize text to speech(voice)
 engine = pyttsx3.init()
-voices = engine.getProperty('voices')
 
 
-def speak_es(audio):
+def speak_es(audio_data):
     """
     Spanish voice
-    :param audio:
+    :param audio_data:
     :return: string
     """
-
     es_voice_id = "com.apple.speech.synthesis.voice.monica"
     engine.setProperty('voice', es_voice_id)
-    engine.say(audio)
+    engine.say(audio_data)
     engine.runAndWait()
 
 
-def speak_de(audio):
+def speak_de(audio_data):
     """
     German voice
-    :param audio:
+    :param audio_data:
     :return: string
     """
     de_voice_id = "com.apple.speech.synthesis.voice.anna"
     engine.setProperty('voice', de_voice_id)
-    engine.say(audio)
+    engine.say(audio_data)
     engine.runAndWait()
 
 
-with mic as source:
-    # Speak the assistant with the Spanish Voice
-    speak_es('Hola, dime lo que quieres traducir')
-    # listen the audio via source
-    r.adjust_for_ambient_noise(source, 5)
-    r.pause_threshold = 1
-    audio = r.listen(source)
-    # define the input language (Spanish)
-    result = r.recognize_google(audio, language='es')
-    speak_es('quieres traducir: ' + result + 'en aleman')
-    # initialize the translator
-    p = google_translator()
-    k = p.translate(result, lang_tgt='de')
-    # German  Voice
-    speak_de(k)
+def speak_en(audio_data):
+    en_voice_id = 'com.apple.speech.synthesis.voice.Victoria'
+    engine.setProperty('voice', en_voice_id)
+    engine.say(audio_data)
+    engine.runAndWait()
 
+
+def recognize_voice():
+    with mic as source:
+        # Speak the assistant with the Spanish Voice
+        speak_en('hello, how can I help you?')
+        audio_data = ''
+        # listen the audio via source
+        r.adjust_for_ambient_noise(source)
+        voice = r.listen(source)
+        try:
+            # define the input language (Spanish)
+            audio_data = r.recognize_google(voice, language='en-in')
+            print(audio_data)
+            if 'yes' in audio_data:
+                speak_en('works fine!!')
+        except sr.RequestError:
+            speak_en('check your internet conetion')
+        except sr.UnknownValueError:
+            speak_en('i dont get  that')
+        return audio_data.lower()
+
+
+sleep(1)
+if __name__ == '__main__':
+    recognize_voice()
