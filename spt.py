54338dfe1e2e6c09fb917000c9399262fd4da69d
Brabitt <ronnyanchaluisa@gmail.com>
2021-05-10 16:48:57 +0200

    added a class Assistant

--- spt.py
+++ spt.py
@@ -5,10 +5,6 @@ from time import sleep
 import datetime
 
 
-# initialize the microphone of the computer
-r = sr.Recognizer()
-mic = sr.Microphone(device_index=0)
-
 # initialize text to speech(voice)
 engine = pyttsx3.init()
 
@@ -44,28 +40,32 @@ def speak_en(audio_data):
     engine.runAndWait()
 
 
-def recognize_voice():
-    with mic as source:
-        # Speak the assistant with the Spanish Voice
-        speak_en('hello, how can I help you?')
-        audio_data = ''
-        # listen the audio via sourcebumb
-        r.adjust_for_ambient_noise(source)
-        voice = r.listen(source)
-        try:
-            # define the input language (Spanish)
-            audio_data = r.recognize_google(voice, language='en-in')
-            print(audio_data)
-            if 'what time is it' in audio_data:
-                print(datetime.datetime.now())
-                speak_en(str(datetime.datetime.now()))
-        except sr.RequestError:
-            speak_en('check your internet connection')
-        except sr.UnknownValueError:
-            speak_en('I do not get that')
-        return audio_data.lower()
+class Assistant:
+    def __init__(self, r, mic):
+        self.r = r
+        self.mic = mic
+
+    def recognize_voice(self):
+        with self.mic as source:
+            print('listening....')
+            # listen the audio via source
+            self.r.adjust_for_ambient_noise(source)
+            voice = self.r.listen(source)
+            try:
+                # define the input language (English)
+                audio_data = self.r.recognize_google(voice, language='en-in')
+                print(audio_data)
+                if 'what time is it' in audio_data:
+                    print(datetime.datetime.now())
+            except sr.RequestError:
+                speak_en('check your internet connection')
+            except sr.UnknownValueError:
+                speak_en('I do not get that')
+            return audio_data.lower()
 
 
 sleep(1)
 if __name__ == '__main__':
-    recognize_voice()
+    # initialize the microphone of the computer
+    x = Assistant(sr.Recognizer(), sr.Microphone(device_index=0))
+    print(x.recognize_voice())
