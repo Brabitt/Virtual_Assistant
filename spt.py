2ad84510c7beaaef2aa9ada9c2b7d703530c27f6
Brabitt <ronnyanchaluisa@gmail.com>
2021-05-12 15:34:32 +0200

    added a class Voice

--- spt.py
+++ spt.py
@@ -1,71 +1,69 @@
 import speech_recognition as sr
-from google_trans_new import google_translator
 import pyttsx3
 from time import sleep
 import datetime
 
 
-# initialize text to speech(voice)
-engine = pyttsx3.init()
+class Voice:
+    def __init__(self):
+        self.engine = pyttsx3.init()
+
+    def speak_es(self, audio):
+        """
+
+        :param audio:
+        :return:audio
+
+        """
+        es_voice_id = "com.apple.speech.synthesis.voice.monica"
+        self.engine.setProperty('voice', es_voice_id)
+        self.engine.setProperty('volume', 0.9)
+        self.engine.say(audio)
+        self.engine.runAndWait()
+        return audio
+
+    def speak_en(self, audio):
+        """
+
+        :param audio:
+        :return:audio
+
+        """
+        en_voice_id = 'com.apple.speech.synthesis.voice.Victoria'
+        self.engine.setProperty('voice', en_voice_id)
+        self.engine.say(audio)
+        self.engine.runAndWait()
+        return audio
 
 
-def speak_es(audio_data):
-    """
-    Spanish voice
-    :param audio_data:
-    :return: string
-    """
-    es_voice_id = "com.apple.speech.synthesis.voice.monica"
-    engine.setProperty('voice', es_voice_id)
-    engine.say(audio_data)
-    engine.runAndWait()
+class Assistant(Voice):
+    def __init__(self):
+        super().__init__()
+        self.r = sr.Recognizer()
+        self.mic = sr.Microphone(device_index=0)
 
-
-def speak_de(audio_data):
-    """
-    German voice
-    :param audio_data:
-    :return: string
-    """
-    de_voice_id = "com.apple.speech.synthesis.voice.anna"
-    engine.setProperty('voice', de_voice_id)
-    engine.say(audio_data)
-    engine.runAndWait()
-
-
-def speak_en(audio_data):
-    en_voice_id = 'com.apple.speech.synthesis.voice.Victoria'
-    engine.setProperty('voice', en_voice_id)
-    engine.say(audio_data)
-    engine.runAndWait()
-
-
-class Assistant:
-    def __init__(self, r, mic):
-        self.r = r
-        self.mic = mic
-
-    def recognize_voice(self):
+    def recognize_voice(self, y=Voice()):
         with self.mic as source:
             print('listening....')
             # listen the audio via source
             self.r.adjust_for_ambient_noise(source)
-            voice = self.r.listen(source)
+            self.r.dynamic_energy_threshold = 3000
+            voice = self.r.listen(source, timeout=5.0)
             try:
+
                 # define the input language (English)
                 audio_data = self.r.recognize_google(voice, language='en-in')
                 print(audio_data)
                 if 'what time is it' in audio_data:
-                    print(datetime.datetime.now())
+                    y.speak_en(str(datetime.datetime.now()))
             except sr.RequestError:
-                speak_en('check your internet connection')
+                y.speak_en('check your internet connection')
             except sr.UnknownValueError:
-                speak_en('I do not get that')
-            return audio_data.lower()
+                y.speak_en('I do not get that')
 
 
 sleep(1)
 if __name__ == '__main__':
     # initialize the microphone of the computer
-    x = Assistant(sr.Recognizer(), sr.Microphone(device_index=0))
-    print(x.recognize_voice())
+    x = Assistant()
+    x.recognize_voice()
