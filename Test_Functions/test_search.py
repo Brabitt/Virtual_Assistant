79322cba85a14a35bac94bfd06b228a7a8ff93f6
Brabitt <ronnyanchaluisa@gmail.com>
2021-05-17 15:50:40 +0200

    added unit test

--- Test_Functions/test_search.py
+++ Test_Functions/test_search.py
@@ -1,14 +1,17 @@
-from SPT.spt import Assistant, Voice
+import unittest
+from Functions.search import Search
 import webbrowser as wb
+from SPT.spt import Voice, Assistant
 
 
-class Search(Assistant, Voice):
-    def __init__(self):
-        super().__init__()
+class test_Search_Google(unittest.TestCase, Search):
+
+    def test_search_google_assert(self):
+        self.assertTrue(test_Search_Google)
+        print('testing the search functions....')
+
+    def test_search_google(self, voices=Voice(), x=Assistant()):
         self.open = wb.open
-
-    # method for search on google
-    def search_google(self, voices=Voice(), x=Assistant()):
         print(voices.speak_en('tell what do you want to search '))
         assistant = x.recognize_voice()
         url = 'https://google.com/search?q=' + assistant
@@ -16,11 +19,15 @@ class Search(Assistant, Voice):
         print(voices.speak_en('this is what a found for ' + assistant))
         return new_url
 
-    # method for search on YouTube
-    def search_youtube(self, voices=Voice(), x=Assistant()):
+    def test_search_youtube(self, voices=Voice(), x=Assistant()):
+        self.open = wb.open
         print(voices.speak_en('tell what do you want to search on youtube '))
         assistant = x.recognize_voice()
         url = 'https://www.youtube.com/results?search_query=' + assistant
         new_url = self.open(url)
         print(voices.speak_en('this is what a found for ' + assistant))
         return new_url
+
+
+if __name__ == '__main__':
+    unittest.main()
--- app.py
+++ app.py
@@ -5,23 +5,27 @@ from Functions.search import Search
 from Functions.time import Time
 
 
-x = Assistant()
-assistant = x.recognize_voice()
+while True:
 
+    x = Assistant()
+    assistant = x.recognize_voice()
+    if __name__ == '__main__':
+        if assistant in translator:
+            x = Translator()
+            x.Spanish()
 
-if __name__ == '__main__':
-    if assistant in translator:
-        x = Translator()
-        x.Spanish()
+        elif assistant in time:
+            z = Time()
+            z.Hour()
 
-    elif assistant in time:
-        z = Time()
-        z.Hour()
+        elif assistant in google:
+            y = Search()
+            y.search_google()
 
-    elif assistant in google:
-        y = Search()
-        y.search_google()
+        elif assistant in youtube:
+            h = Search()
+            h.search_youtube()
 
-    elif assistant in youtube:
-        h = Search()
-        h.search_youtube()
+        else:
+            if 'exit' in assistant:
+                break
